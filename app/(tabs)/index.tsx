e1f98410d55c5c65f5bdc87f89e6ff82b8b30648
Calvin Lua <86276701+calvinlua@users.noreply.github.com>
2025-02-03 14:19:04 +0800

    Add image viewer directly from library.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,39 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, ImageComponent } from "react-native";
 import { Link } from "expo-router";
+import { Image } from "expo-image";
+
+const placeHolderImage = require("@/assets/images/background-image.png");
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
-    justifyContent: "center",
   },
-  text: {
-    color: "#fff",
+  // text: {
+  //   color: "#fff",
+  // },
+  // button: {
+  //   fontSize: 20,
+  //   textDecorationLine: "underline",
+  //   color: "#fff",
+  // },
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
+      <View style={styles.container}>
+        <Image source={placeHolderImage} style={styles.image}></Image>
+      </View>
     </View>
   );
 }
