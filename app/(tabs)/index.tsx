784b3d831e9018c61a6e904159a368edb8130a54
Calvin Lua <86276701+calvinlua@users.noreply.github.com>
2025-02-03 18:50:46 +0800

    Added theme for button for primary theme , update "choose a photo" button to use primary themed button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -27,7 +27,7 @@ export default function Index() {
         <ImageViewer imgSource={placeHolderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button label={"Choose a photo"} />
+        <Button label={"Choose a photo"} theme="primary" />
         <Button label={"Use this photo"} />
       </View>
     </View>
