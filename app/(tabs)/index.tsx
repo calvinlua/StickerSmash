662ef85ef988e069ab00896ef333057bf31f41a8
Calvin Lua <86276701+calvinlua@users.noreply.github.com>
2025-02-03 18:31:52 +0800

    Update common component Button and update home tab with common Button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
-import { Text, View, StyleSheet, ImageComponent } from "react-native";
-import { Link } from "expo-router";
-import { Image } from "expo-image";
+import { View, StyleSheet } from "react-native";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
 
 const placeHolderImage = require("@/assets/images/background-image.png");
 
@@ -10,29 +10,25 @@ const styles = StyleSheet.create({
     backgroundColor: "#25292e",
     alignItems: "center",
   },
-  // text: {
-  //   color: "#fff",
-  // },
-  // button: {
-  //   fontSize: 20,
-  //   textDecorationLine: "underline",
-  //   color: "#fff",
-  // },
   imageContainer: {
     flex: 1,
+    paddingTop: 28,
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <View style={styles.container}>
-        <Image source={placeHolderImage} style={styles.image}></Image>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={placeHolderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label={"Choose a photo"} />
+        <Button label={"Use this photo"} />
       </View>
     </View>
   );
