fb271cea71faaac95398c66f6b458bdba34efcd0
Calvin Lua <86276701+calvinlua@users.noreply.github.com>
2025-02-03 14:22:51 +0800

    Create Common Component ImageViewer from expo SDK

---
+++ components/ImageViewer.tsx
@@ -0,0 +1,19 @@
+import { Image, type ImageSource } from "expo-image";
+import { StyleSheet } from "react-native";
+
+//declare type
+type Props = {
+  imgSource: ImageSource;
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
+
+export default function ImageViewer({ imgSource }: Props) {
+  return <Image source={imgSource} style={}></Image>;
+}
