f6f6c9e8f82413974455411f8d44ed410418056b
Calvin Lua <86276701+calvinlua@users.noreply.github.com>
2025-02-05 08:07:48 +0800

    Update modal emoji picker on syntax error

--- components/EmojiPicker.tsx
+++ components/EmojiPicker.tsx
@@ -38,15 +38,13 @@ export default function EmojiPicker({ isVisible, onClose, children }: Props) {
     <Modal animationType="slide" transparent={true} visible={isVisible}>
       <View style={styles.modalContent}>
         <View style={styles.titleContainer}>
-          <Text style={styles.title} /> 
-          Choose a sticker 
-          </Text>
+          <Text style={styles.title}>Choose a sticker</Text>
           <Pressable onPress={onClose}>
             <MaterialIcons name="close" color="#fff" size={22} />
           </Pressable>
         </View>
-          {children}
+        {children}
       </View>
-  </Modal>
+    </Modal>
   );
 }
