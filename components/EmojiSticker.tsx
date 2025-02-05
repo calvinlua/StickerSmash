bef399b3050d741c541dabdc7f59df65e66cec82
Calvin Lua <86276701+calvinlua@users.noreply.github.com>
2025-02-05 18:34:46 +0800

    Add double tap feature to enlarge sticker by 2 times

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,6 +1,11 @@
 import { ImageSource } from "expo-image";
 import { View } from "react-native";
-import Animated from "react-native-reanimated";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 
 type Props = {
   imageSize: number;
@@ -8,13 +13,36 @@ type Props = {
 };
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
+  const scaleImage = useSharedValue(imageSize); //  scaling on sticker
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart(() => {
+      //double tap action initialize
+      if (scaleImage.value !== imageSize * 2) {
+        scaleImage.value = scaleImage.value * 2;
+      } else {
+        scaleImage.value = Math.round(scaleImage.value / 2);
+      }
+    });
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      // withSpring got spring-based animation transition
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    };
+  });
   return (
     //temporaily put the sticker image on -350px of the selected image
-    <View style={{ top: -350, backgroundColor: "purple" }}>
-      <Animated.Image
-        source={stickerSource}
-        style={{ width: imageSize, height: imageSize }}
-      />
+    <View style={{ top: -350 }}>
+      <GestureDetector gesture={doubleTap}>
+        {/* Animated component loopks at style prop to apply updates for animation */}
+        {/* [] allow overidding */}
+        <Animated.Image
+          source={stickerSource}
+          resizeMode={"contain"}
+          style={[imageStyle, { width: imageSize, height: imageSize }]}
+        />
+      </GestureDetector>
     </View>
   );
 }
