1268176317cdf48a566fcdb13767f64f28b1ac87
Calvin Lua <86276701+calvinlua@users.noreply.github.com>
2025-02-07 07:01:39 +0800

    Update app
    Update logo, fix save image for web platform and add light theme status bar for home tab page.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,19 @@
-import { View, StyleSheet } from "react-native";
-import ImageViewer from "@/components/ImageViewer";
-import Button from "@/components/Button";
+import { View, StyleSheet, Platform } from "react-native";
 import * as ImagePicker from "expo-image-picker";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { ImageSource } from "expo-image";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as MediaLibrary from "expo-media-library";
+import { captureRef } from "react-native-view-shot";
+import domtoimage from "dom-to-image";
+
 import IconButton from "@/components/IconButton";
 import CircleButton from "@/components/CircleButton";
 import EmojiPicker from "@/components/EmojiPicker";
 import EmojiList from "@/components/EmojiList";
-import { ImageSource } from "expo-image";
 import EmojiSticker from "@/components/EmojiSticker";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
 
 const placeHolderImage = require("@/assets/images/background-image.png");
 
@@ -38,6 +42,7 @@ const styles = StyleSheet.create({
 });
 
 export default function Index() {
+  const [status, requestPermission] = MediaLibrary.usePermissions();
   const [selectedImage, setSelectedImage] = useState<string | undefined>(
     undefined
   );
@@ -47,9 +52,16 @@ export default function Index() {
     undefined
   );
 
+  const imageRef = useRef<View>(null);
+
+  if (status === null) {
+    requestPermission(); //request permission for use of media library
+  }
+
   const onReset = () => {
     setShowAppOptions(false);
     setSelectedImage(undefined);
+    setPickedEmoji(undefined);
   };
   const onAddSticker = () => {
     setIsModalVisible(true);
@@ -59,9 +71,6 @@ export default function Index() {
     setIsModalVisible(false);
   };
 
-  const onSaveImageAsync = async () => {
-    //implementLater
-  };
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ["images"],
@@ -79,17 +88,55 @@ export default function Index() {
     }
   };
 
+  const onSaveImageAsync = async () => {
+    if (Platform.OS !== "web") {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert("Saved!");
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      //on web
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 1,
+          width: 320,
+          height: 440,
+        });
+        let link = document.createElement("a");
+        link.download = "edited-picture.jpeg";
+        //embed the uri into hyperlink
+        link.href = dataUrl;
+        // simulate a click
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
   return (
     <GestureHandlerRootView>
       <View style={styles.container}>
         <View style={styles.imageContainer}>
-          <ImageViewer
-            imgSource={placeHolderImage}
-            selectedImage={selectedImage}
-          />
-          {pickedEmoji && (
-            <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
-          )}
+          {/* collaspsable set to false so that background and emoji are not removed when unused */}
+          <View ref={imageRef} collapsable={false}>
+            <ImageViewer
+              imgSource={placeHolderImage}
+              selectedImage={selectedImage}
+            />
+            {pickedEmoji && (
+              <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+            )}
+          </View>
         </View>
         {showAppOptions ? (
           <View style={styles.optionsContainer}>
